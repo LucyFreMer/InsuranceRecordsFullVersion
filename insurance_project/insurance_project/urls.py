884e7112c99ff5af58e35174adb470ca2b3390b0
LucyFreMer <lucyfremer@gmail.com>
2024-08-06 10:31:48 +0200

    changing urls and settings

--- insurance_project/insurance_project/urls.py
+++ insurance_project/insurance_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pojisteni/', include('insurance.urls')),
+    path('', include('insurance.urls')),
 ]
